72f7567a3d4e16fe200df71d2f66e77c5e7092ba
Peihan Wang <wang.peiha@northeastern.edu>
2025-11-05 12:04:13 -0800

    Update CODE.py
    Pseudocode

--- CODE.py
+++ CODE.py
@@ -1,1 +1,16 @@
+procedure insertionSort(books)
+    for i ← 1 to length(books) - 1 do
+        key ← books[i]
+        j ← i - 1
+        
+        while j ≥ 0 and 
+              (books[j].shelf > key.shelf OR
+              (books[j].shelf = key.shelf AND books[j].order > key.order)) do
+            books[j + 1] ← books[j]
+            j ← j - 1
+        end while
+        
+        books[j + 1] ← key
+    end for
+end procedure
 
